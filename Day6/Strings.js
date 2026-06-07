bb31e2a81ae608db2db4760134de3e1a515edf2e
Pranav Patil <patilpranav1117@gmail.com>
2026-06-07 23:57:27 +0530

    Day6

---
+++ Day6/Strings.js
@@ -0,0 +1,24 @@
+const name = "Pranav"
+console.log(name);
+
+console.log(name[0]);
+
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.charAt(4));
+console.log(name.indexOf('a'));
+
+const newName = new String("Patil")
+console.log(newName);
+
+console.log(`My name is ${name} ${newName}`);
+
+const newstring = name.substring(0, 3);
+console.log(newstring);
+
+const raw = name.slice(1, -2);
+console.log(raw);
+
+const myName = "   Pranav Patil   "
+console.log(myName);
+console.log(myName.trim());
